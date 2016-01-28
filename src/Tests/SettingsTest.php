6f893b65300652fa327ea88a4562814a46ca421d
Erik Wegner <e_wegner@web.de>
2016-01-28 20:53:36 +0100

    More basic tests

--- src/Tests/SettingsTest.php
+++ src/Tests/SettingsTest.php
@@ -3,6 +3,7 @@
 namespace Drupal\nagios\Tests;
 
 use Drupal\simpletest\WebTestBase;
+use Drupal\Core\Session\AccountInterface;
 
 /**
  * Tests the settings functionality
@@ -20,26 +21,80 @@ class SettingsTest extends WebTestBase {
 
   /**
    * A simple user with 'administer site configuration' permission
+   * @var AccountInterface
    */
-  private $user;
+  private $settingsUser;
+
+  /**
+   * A user with 'administer nagios ignore' permission
+   * @var AccountInterface 
+   */
+  private $modulesUser;
+
+  /**
+   * Url to the settings page
+   */
+  const SETTINGS_PATH = 'admin/config/system/nagios';
+
+  /**
+   * Url to the ignored modules page
+   */
+  const IGNORED_MODULES_PATH = 'admin/config/system/nagios/ignoredmodules';
 
   /**
    * Perform any initial set up tasks that run before every test method
    */
   public function setUp() {
     parent::setUp();
-    $this->user = $this->drupalCreateUser(array('administer site configuration'));
+    $this->settingsUser = $this->drupalCreateUser(array('administer site configuration'));
+    $this->modulesUser = $this->drupalCreateUser(array('administer nagios ignore'));
   }
 
   /**
    * Tests that the 'admin/config/system/nagios' path returns the right content
    */
-  public function testCustomPageExists() {
-    $this->drupalLogin($this->user);
+  public function testSettingsPageExists() {
+    $this->drupalLogin($this->settingsUser);
 
-    $this->drupalGet('admin/config/system/nagios');
+    $this->drupalGet(SettingsTest::SETTINGS_PATH);
     $this->assertResponse(200);
     $this->assertText('Unique ID');
   }
+  
+  /**
+   * Tests that the 'admin/config/system/nagios/ignoredmodules' path returns the right content
+   */
+  public function testIgnoredModulesPageExists() {
+    $this->drupalLogin($this->modulesUser);
 
+    $this->drupalGet(SettingsTest::IGNORED_MODULES_PATH);
+    $this->assertResponse(200);
+    $this->assertText('Select those modules that should be ignored for requirement checks.');
+  }
+
+  /**
+   * Test required permissions for the settings page
+   */
+  public function testSettingsPagePermissions() {
+    $this->drupalLogin($this->settingsUser);
+    $this->drupalGet(SettingsTest::SETTINGS_PATH);
+    $this->assertResponse(200);
+
+    $this->drupalLogin($this->modulesUser);
+    $this->drupalGet(SettingsTest::SETTINGS_PATH);
+    $this->assertResponse(403);
+  }
+
+  /**
+   * Test required permissions for the page 'Ignored modules'
+   */
+  public function testIgnoredModulesPagePermissions() {
+    $this->drupalLogin($this->settingsUser);
+    $this->drupalGet(SettingsTest::IGNORED_MODULES_PATH);
+    $this->assertResponse(403);
+
+    $this->drupalLogin($this->modulesUser);
+    $this->drupalGet(SettingsTest::IGNORED_MODULES_PATH);
+    $this->assertResponse(200);
+  }
 }
